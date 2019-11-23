2b682982ab0c964a0387a4027f5b5864975edc2e
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-11-23 20:27:01 +0700

    URL thân thiện

--- MultiShop/MultiShop/App_Start/RouteConfig.cs
+++ MultiShop/MultiShop/App_Start/RouteConfig.cs
@@ -13,17 +13,37 @@ namespace MultiShop
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            
-
-            routes.MapRoute(
-                name: "chi-tiet",
-                url: "chi-tiet/{name}_{id}",
-                defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
-                namespaces: new[] { "MultiShop.Controllers" }
-                
-            );
-
-            routes.MapRoute(
+			routes.MapRoute(
+				name: "san-pham",
+				url: "san-pham/{metatile}-{CategoryID}",
+				defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional },
+				namespaces: new[] { "MultiShop.Controllers" }
+			);
+			routes.MapRoute(
+				name: "chi-tiet-san-pham",
+				url: "chi-tiet-san-pham/{name}_{id}",
+				defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional },
+				namespaces: new[] { "MultiShop.Controllers" }
+			);
+			routes.MapRoute(
+				name: "gio-hang",
+				url: "gio-hang",
+				defaults: new { controller = "Cart", action = "Index", id = UrlParameter.Optional },
+				namespaces: new[] { "MultiShop.Controllers" }
+			);
+			routes.MapRoute(
+				name: "dang-ky",
+				url: "dang-ky",
+				defaults: new { controller = "Account", action = "Register", id = UrlParameter.Optional },
+				namespaces: new[] { "MultiShop.Controllers" }
+			);
+			routes.MapRoute(
+				name: "thanh-toan",
+				url: "thanh-toan",
+				defaults: new { controller = "Order", action = "Checkout", id = UrlParameter.Optional },
+				namespaces: new[] { "MultiShop.Controllers" }
+			);
+			routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
