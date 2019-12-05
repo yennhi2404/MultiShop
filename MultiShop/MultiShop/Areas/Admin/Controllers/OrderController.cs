3ff5607daba2a239628e783199e469e56bbec99c
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-12-05 20:23:04 +0700

    Manage user order

---
+++ MultiShop/MultiShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiShop.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private MultiShopDbContext db = new MultiShopDbContext();
+
+        // GET: Admin/Order
+        public ActionResult Index()
+        {
+            var orderDetails = db.OrderDetails.Include(o => o.Order).Include(o => o.Product);
+            return View(orderDetails.ToList());
+        }
+
+        // GET: Admin/Order/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            if (orderDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderDetail);
+        }
+
+        // GET: Admin/Order/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            if (orderDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "CustomerId", orderDetail.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", orderDetail.ProductId);
+            return View(orderDetail);
+        }
+
+        // POST: Admin/Order/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,OrderId,ProductId,UnitPrice,Quantity,Discount")] OrderDetail orderDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.OrderId = new SelectList(db.Orders, "Id", "CustomerId", orderDetail.OrderId);
+            ViewBag.ProductId = new SelectList(db.Products, "Id", "Name", orderDetail.ProductId);
+            return View(orderDetail);
+        }
+
+        // GET: Admin/Order/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            if (orderDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderDetail);
+        }
+
+        // POST: Admin/Order/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            db.OrderDetails.Remove(orderDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
