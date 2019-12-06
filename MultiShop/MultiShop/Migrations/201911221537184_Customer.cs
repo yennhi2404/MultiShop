36f037018f413de4b87538d0fe0547f94ac21525
MinhPhuc1998 <44628658+MinhPhuc1998@users.noreply.github.com>
2019-12-06 18:39:16 +0700

    Export user to xlsx file

---
+++ MultiShop/MultiShop/Migrations/201911221537184_Customer.cs
@@ -0,0 +1,18 @@
+namespace MultiShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Customer : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.AspNetUsers", "UserName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.AspNetUsers", "UserName", c => c.String());
+        }
+    }
+}
