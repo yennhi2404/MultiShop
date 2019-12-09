e17fb6e8e23f48497b9ad1e1942056933fc41206
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-12-09 21:58:39 +0700

    Paypal

--- MultiShop/MultiShop/Global.asax.cs
+++ MultiShop/MultiShop/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MultiShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -11,13 +13,14 @@ namespace MultiShop
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-    }
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			log4net.Config.XmlConfigurator.Configure();
+		}
+	}
 }
